616c470a03e732976fd4573c022ad6d6016db368
Priyammm05 <priyam05soni@gmail.com>
2024-04-07 14:09:35 +0530

    Added the LocalFileManager to save the images in the File Manager

---
+++ crypto-app/Utilities/LocalFileManager.swift
@@ -0,0 +1,70 @@
+//
+//  LocalFileManager.swift
+//  crypto-app
+//
+//  Created by Priyam Soni on 07/04/24.
+//
+
+import Foundation
+import SwiftUI
+
+class LocalFileManager{
+    static let instance = LocalFileManager()
+    private init(){}
+    
+    func saveImage(image: UIImage, imageName: String, folderName: String){
+        
+        createFolderIfNeeded(folderName: folderName)
+        
+        guard
+            let data = image.pngData(),
+            let url = getUrlForImage(imageName: imageName, folderName: folderName)
+        
+        else { return }
+        
+        do{
+            try data.write(to: url)
+        }catch let error {
+            print("Error saving image. ImageName: \(imageName). \(error)")
+        }
+    }
+    
+    func getImage(imageName: String, folderName: String) -> UIImage? {
+        guard
+            let url = getUrlForImage(imageName: imageName, folderName: folderName),
+            FileManager.default.fileExists(atPath: url.path)
+        else { return nil }
+        
+        return UIImage(contentsOfFile: url.path)
+    }
+    
+    private func createFolderIfNeeded(folderName: String){
+        guard
+            let url = getUrlForFolder(folderName: folderName)
+        else { return }
+        
+        if !FileManager.default.fileExists(atPath: url.path){
+            do {
+                try FileManager.default.createDirectory(at: url, withIntermediateDirectories: true, attributes: nil)
+            } catch let error {
+                print("Error creating directory. FolderName: \(folderName). \(error)")
+            }
+        }
+    }
+    
+    private func getUrlForFolder(folderName: String) -> URL? {
+        guard
+            let url = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first
+        else { return nil }
+        
+        return url.appendingPathComponent(folderName)
+    }
+    
+    private func getUrlForImage(imageName: String, folderName: String) -> URL? {
+        guard
+            let folderUrl = getUrlForFolder(folderName: folderName)
+        else { return nil }
+        
+        return folderUrl.appendingPathComponent(imageName + ".png")
+    }
+}
