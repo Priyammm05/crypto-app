0fddec1d55192093543a873ea5921878dc699ed2
Priyammm05 <priyam05soni@gmail.com>
2024-04-09 12:54:02 +0530

    Added the portfolio view and xmarkbutton component

---
+++ crypto-app/Core/Home/View/PortfolioView.swift
@@ -0,0 +1,18 @@
+//
+//  PortfolioView.swift
+//  crypto-app
+//
+//  Created by Priyam Soni on 09/04/24.
+//
+
+import SwiftUI
+
+struct PortfolioView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PortfolioView()
+}
