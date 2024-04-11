577cfd2bd3b465b860907c3d6fb9b802ebbe2281
Priyammm05 <priyam05soni@gmail.com>
2024-04-11 16:59:41 +0530

    Updated the Details View with the Chart View

--- crypto-app/Core/Details/View/DetailView.swift
+++ crypto-app/Core/Details/View/DetailView.swift
@@ -13,14 +13,22 @@ struct DetailLoadingView: View{
     var body: some View{
         ZStack{
             if let coin = coin{
-                Text(coin.name)
+                DetailView(coin: coin)
             }
         }
     }
 }
 
 struct DetailView: View {
-    @StateObject var viewModel: DetailViewModel
+    @StateObject private var viewModel: DetailViewModel
+    
+    private let columns: [GridItem] = [
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    
+    private let spacing: CGFloat = 30
+    
     let coin: CoinModel
     
     init(coin: CoinModel) {
@@ -29,7 +37,29 @@ struct DetailView: View {
     }
     
     var body: some View {
-        Text(coin.name)
+        ScrollView{
+            VStack {
+                ChartView(coin: viewModel.coin)
+                    .padding(.vertical)
+                
+                VStack(spacing: 20){
+                    overviewTitle
+                    Divider()
+                    overviewGrid
+                    
+                    additionalTitle
+                    Divider()
+                    additionalGrid
+                }
+                .padding()
+            }
+        }
+        .navigationTitle(viewModel.coin.name)
+        .toolbar{
+            ToolbarItem(placement: .navigationBarTrailing) {
+                navigationBarTrailingItem
+            }
+        }
     }
 }
 
@@ -37,6 +67,63 @@ struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
         NavigationView {
             DetailView(coin: dev.coin)
+        }
+    }
+}
+
+extension DetailView{
+    private var overviewTitle: some View{
+        Text("Overview")
+            .font(.title)
+            .bold()
+            .foregroundColor(Color.theme.accent)
+            .frame(maxWidth: .infinity, alignment: .leading)
+    }
+    
+    private var additionalTitle: some View{
+        Text("Additinal Details")
+            .font(.title)
+            .bold()
+            .foregroundColor(Color.theme.accent)
+            .frame(maxWidth: .infinity, alignment: .leading)
+    }
+    
+    private var overviewGrid: some View{
+        LazyVGrid(
+            columns: columns,
+            alignment: .leading,
+            spacing: spacing,
+            pinnedViews: [],
+            content: {
+                ForEach(viewModel.overviewStatistics){ stat in
+                    StatisticView(stat: stat)
+                }
+            }
+        )
+    }
+    
+    private var additionalGrid: some View{
+        LazyVGrid(
+            columns: columns,
+            alignment: .leading,
+            spacing: spacing,
+            pinnedViews: [],
+            content: {
+                ForEach(viewModel.additionalStatistics){ stat in
+                    StatisticView(stat: stat)
+                }
+            }
+        )
+    }
+    
+    private var navigationBarTrailingItem: some View{
+        HStack{
+            Text(viewModel.coin.symbol.uppercased())
+                .font(.headline)
+                .foregroundColor(Color.theme.secondaryText)
+            
+            CoinImageView(coin: viewModel.coin)
+                .frame(width: 25, height: 25)
         }
     }
 }
