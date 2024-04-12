0bde0c07a7bf751565d8289bfa08aad78b6aad0c
Priyammm05 <priyam05soni@gmail.com>
2024-04-12 15:27:03 +0530

    Updated the entry main app and added the LaunchView

--- crypto-app/crypto_appApp.swift
+++ crypto-app/crypto_appApp.swift
@@ -10,6 +10,7 @@ import SwiftUI
 @main
 struct crypto_appApp: App {
     @StateObject private var vm = HomeViewModel()
+    @State private var showLaunchView: Bool = true
     
     init(){
         UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: UIColor(Color.theme.accent)]
@@ -20,11 +21,22 @@ struct crypto_appApp: App {
     
     var body: some Scene {
         WindowGroup {
-            NavigationView{
-                HomeView()
-                    .navigationBarHidden(true)
+            ZStack{
+                NavigationView{
+                    HomeView()
+                        .navigationBarHidden(true)
+                }
+                .navigationViewStyle(StackNavigationViewStyle())
+                .environmentObject(vm)
+                
+                ZStack {
+                    if showLaunchView {
+                        LaunchView(showLaunchView: $showLaunchView)
+                            .transition(.move(edge: .leading))
+                    }
+                }
+                .zIndex(2.0)
             }
-            .environmentObject(vm)
         }
     }
 }
